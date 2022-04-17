9ba46960e9c2a12d3599a6e34282c1e1ed2c4abf
noambenshoham <102994352+noambenshoham@users.noreply.github.com>
2022-04-18 02:52:42 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,28 +1,74 @@
-window.addEventListener('load', () => {
-    const board = document.createElement("table");
-    board.classList.add("chess");
+function Chess() {
+    const BOARD_SIZE = 8;
+    const boardEl = document.createElement("table");
+    boardEl.classList.add("chessBoard");
 
-    document.body.appendChild(board);
-    for (let x = 0; x < 8; x++) {
-        const line = document.createElement("tr");
-        board.appendChild(line);
-        for (let y = 0; y < 4; y++) {
-            if (x % 2 === 0) {
-                const whiteSqare = document.createElement("th");
-                line.appendChild(whiteSqare)
-                whiteSqare.classList.add("white");
-                const blackSquare = document.createElement("th");
-                blackSquare.classList.add("black");
-                line.appendChild(blackSquare)
+    document.body.appendChild(boardEl);
+    for (let x = 0; x < BOARD_SIZE; x++) {
+        let row = boardEl.insertRow();
+        for (let y = 0; y < BOARD_SIZE; y++) {
+            let cell = row.insertCell();
+            cell.innerHTML = addImg(x, y);
+            cell.addEventListener('click', () => cell.classList.add("clicked"));
+            if ((x + y) % 2 === 0) {
+                cell.classList.add("white");
             } else {
-                const blackSquare = document.createElement("th");
-                blackSquare.classList.add("black");
-                line.appendChild(blackSquare)
-                const whiteSqare = document.createElement("th");
-                line.appendChild(whiteSqare)
-                whiteSqare.classList.add("white");
+                cell.classList.add("black");
+            }
+            if (x === 0 || x === 1) {
+                cell.classList.add("upSide")
             }
         }
-
     }
-})
+}
+
+window.addEventListener('load', Chess);
+
+
+
+function urlToElement(url) {
+    return "<img src='" + url + "'>"
+}
+
+let blackRook = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Chess_rdt45.svg/68px-Chess_rdt45.svg.png");
+let blackKnight = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Chess_ndt45.svg/68px-Chess_ndt45.svg.png");
+let blackBishop = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Chess_bdt45.svg/68px-Chess_bdt45.svg.png");
+let blackQueen = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Chess_qdt45.svg/68px-Chess_qdt45.svg.png");
+let blackKing = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Chess_kdt45.svg/68px-Chess_kdt45.svg.png");
+let blackPawn = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Chess_pdt45.svg/68px-Chess_pdt45.svg.png");
+let whiteRook = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Chess_rlt45.svg/68px-Chess_rlt45.svg.png");
+let whiteKnight = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Chess_nlt45.svg/68px-Chess_nlt45.svg.png");
+let whiteBishop = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Chess_blt45.svg/68px-Chess_blt45.svg.png");
+let whiteQueen = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Chess_qlt45.svg/68px-Chess_qlt45.svg.png");
+let whiteKing = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Chess_klt45.svg/68px-Chess_klt45.svg.png");
+let whitePawn = urlToElement("https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Chess_plt45.svg/68px-Chess_plt45.svg.png");
+
+function addImg(a, b) {
+    if (a === 1) {
+        return blackPawn
+    } else if ((a === 0 && b === 0) || (a === 0 && b === 7)) {
+        return blackRook;
+    } else if ((a === 0 && b === 1) || (a === 0 && b === 6)) {
+        return blackKnight;
+    } else if ((a === 0 && b === 2) || (a === 0 && b === 5)) {
+        return blackBishop;
+    } else if (a === 0 && b === 3) {
+        return blackQueen;
+    } else if (a === 0 && b === 4) {
+        return blackKing;
+    }
+    if (a === 6) {
+        return whitePawn;
+    } else if ((a === 7 && b === 0) || (a === 7 && b === 7)) {
+        return whiteRook;
+    } else if ((a === 7 && b === 1) || (a === 7 && b === 6)) {
+        return whiteKnight;
+    } else if ((a === 7 && b === 2) || (a === 7 && b === 5)) {
+        return whiteBishop;
+    } else if (a === 7 && b === 3) {
+        return whiteQueen;
+    } else if (a === 7 && b === 4) {
+        return whiteKing;
+    }
+    else return null
+}
